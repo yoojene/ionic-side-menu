2a435865737fbcb4b0f8accf1399b5852b67ff50
Eugene Cross <eugene.cross@gmail.com>
2022-12-05 19:14:33 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'side.menu',
+  appName: 'side-menu',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
